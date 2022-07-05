54af3f3af688895f48fa086935cba991825d8db1
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2022-07-05 18:13:12 +0200

    Added static file server and h5p unpacker

--- AdLerBackend/Program.cs
+++ AdLerBackend/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.Net.Http.Headers;
+using System.IO.Compression;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,6 +38,53 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.UseCors("test");
+
+app.MapPost("/Files/UploadFiles", (HttpRequest request) =>
+{
+    if (!request.Form.Files.Any()) return Results.BadRequest("At least one file is needed");
+
+    foreach (var file in request.Form.Files)
+    {
+
+
+        string fileName = Path.Join("wwwroot", "TestH5P", file.FileName);
+        string rootPath = Path.ChangeExtension(fileName, null);
+        Directory.CreateDirectory(rootPath);
+
+
+        using (var zip = new ZipArchive(file.OpenReadStream(), ZipArchiveMode.Read))
+        {
+            foreach (var entry in zip.Entries)
+            {
+                using (Stream inputStream = entry.Open())
+                {
+                    string filePath = Path.Join(rootPath, entry.FullName);
+                    string? dirName = Path.GetDirectoryName(filePath);
+
+                    if (dirName == null) throw new Exception("dirName is null");
+
+                    Directory.CreateDirectory(dirName);
+                    using (var unpackedFile = File.OpenWrite(filePath))
+                    {
+                        inputStream.CopyTo(unpackedFile);
+                        unpackedFile.Flush();
+                    }
+
+                }
+            }
+        }
+
+
+
+    }
+
+
+
+    return Results.Ok();
+});
+
+
+app.UseStaticFiles();
 
 app.MapControllers();
 

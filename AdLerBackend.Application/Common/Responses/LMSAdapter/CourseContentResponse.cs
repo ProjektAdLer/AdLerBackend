1edba6c9f77bbf19fbf92d8986594aec040d0cba
philgei <philipp@geirhos.com>
2022-09-22 16:25:39 +0200

    removed unused coursecontentresponse

--- AdLerBackend.Application/Common/Responses/LMSAdapter/CourseContentResponse.cs
+++ AdLerBackend.Application/Common/Responses/LMSAdapter/CourseContentResponse.cs
@@ -5,8 +5,3 @@ public class CourseContent
     public int Id { get; set; }
     public string Name { get; set; }
 }
-
-public class CourseContentResponse
-{
-    public List<CourseContent> Courses { get; set; }
-}

67a9ce2f1d039f712387793dbf504bc0415688c2
philgei <philipp@geirhos.com>
2022-09-22 16:18:01 +0200

    removed unused fields from DTOs

--- AdLerBackend.Application/Common/Responses/LMSAdapter/MoodleCourseListResponse.cs
+++ AdLerBackend.Application/Common/Responses/LMSAdapter/MoodleCourseListResponse.cs
@@ -4,24 +4,10 @@ public class MoodleCourseListResponse
 {
     public int Total { get; set; }
     public List<MoodleCourse> Courses { get; set; }
-    public List<object> Warnings { get; set; }
 }
 
 public class MoodleCourse
 {
     public int Id { get; set; }
     public string Fullname { get; set; }
-    public string Displayname { get; set; }
-    public string Shortname { get; set; }
-    public int Categoryid { get; set; }
-    public string Categoryname { get; set; }
-    public int Sortorder { get; set; }
-    public string Summary { get; set; }
-    public int Summaryformat { get; set; }
-    public List<object> Summaryfiles { get; set; }
-    public List<object> Overviewfiles { get; set; }
-    public bool Showactivitydates { get; set; }
-    public bool Showcompletionconditions { get; set; }
-    public List<object> Contacts { get; set; }
-    public List<string> Enrollmentmethods { get; set; }
 }

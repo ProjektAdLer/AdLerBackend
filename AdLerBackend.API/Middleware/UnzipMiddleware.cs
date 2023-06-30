ea4f6b80a0472a96cf2d190728be2e88ae6c0fbf
geirhosp <philipp@geirhos.com>
2023-06-30 21:29:07 +0200

    updated dependencies of unzip middleware

--- AdLerBackend.API/Middleware/UnzipMiddleware.cs
+++ AdLerBackend.API/Middleware/UnzipMiddleware.cs
@@ -1,5 +1,8 @@
+using System;
 using System.IO.Abstractions;
 using System.IO.Compression;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using MimeKit;
 
 namespace AdLerBackend.API.Middleware;

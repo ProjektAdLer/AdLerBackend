5ef11cb4193cd2ea91bfafa1fc6db6751e0b2f18
philgei <philipp@geirhos.com>
2023-07-03 12:22:53 +0200

    added naming attributes to PluginUUIDResponse.cs

--- AdLerBackend.Infrastructure/Moodle/ApiResponses/PluginUUIDResponse.cs
+++ AdLerBackend.Infrastructure/Moodle/ApiResponses/PluginUUIDResponse.cs
@@ -1,10 +1,16 @@
-﻿namespace AdLerBackend.Infrastructure.Moodle.ApiResponses;
+﻿using System.Text.Json.Serialization;
+
+namespace AdLerBackend.Infrastructure.Moodle.ApiResponses;
 
 public class PluginUUIDResponse
 {
-    public string CourseId { get; set; }
-    public string ElementType { get; set; }
-    public string Uuid { get; set; }
-    public int MoodleId { get; set; }
-    public int ContextId { get; set; }
+    [JsonPropertyName("course_id")] public string CourseId { get; set; }
+
+    [JsonPropertyName("element_type")] public string ElementType { get; set; }
+
+    [JsonPropertyName("uuid")] public string Uuid { get; set; }
+
+    [JsonPropertyName("moodle_id")] public int MoodleId { get; set; }
+
+    [JsonPropertyName("context_id")] public int ContextId { get; set; }
 }

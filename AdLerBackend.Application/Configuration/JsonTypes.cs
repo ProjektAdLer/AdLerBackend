6787186847510a848a9de8744b22efd403f4500a
philgei <philipp@geirhos.com>
2023-09-07 11:06:33 +0200

    Add JsonTypes for data deserialization and serialization

---
+++ AdLerBackend.Application/Configuration/JsonTypes.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+
+namespace AdLerBackend.Application.Configuration;
+
+/// <summary>
+///     Json types used for deserialization and serialization
+/// </summary>
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)] // To suppress Rider warnings
+public static class JsonTypes
+{
+    public const string AtfType = "ATF";
+    public const string LearningWorldType = "LearningWorld";
+    public const string LearningTopicType = "LearningTopic";
+    public const string LearningSpaceType = "LearningSpace";
+    public const string LearningElementType = "LearningElement";
+    public const string AdaptivityLearningElementType = "AdaptivityLearningElement";
+    public const string BaseLearningElementType = "BaseLearningElement";
+    public const string AdaptivityContentType = "adaptivityContent";
+    public const string AdaptivityTaskType = "adaptivityTask";
+    public const string AdaptivityQuestionType = "adaptivityQuestion";
+    public const string CorrectnessTriggerType = "correctnessTrigger";
+    public const string AdaptivityCommentActionType = "AdaptivityCommentAction";
+    public const string AdaptivityElementReferenceActionType = "AdaptivityElementReferenceAction";
+    public const string AdaptivityContentReferenceActionType = "AdaptivityContentReferenceAction";
+}

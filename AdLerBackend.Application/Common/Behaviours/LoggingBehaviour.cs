d1b30f809839289733720c4f7e36c5221bc1bb5e
philgei <philipp@geirhos.com>
2024-02-16 11:33:10 +0100

    Removed HTTPContextAccessor from LoggingBehaviour.cs

--- AdLerBackend.Application/Common/Behaviours/LoggingBehaviour.cs
+++ AdLerBackend.Application/Common/Behaviours/LoggingBehaviour.cs
@@ -1,20 +1,16 @@
 ﻿using System.Diagnostics;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace AdLerBackend.Application.Common.Behaviours;
 
 public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
 {
-    private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
 
-    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger,
-        IHttpContextAccessor httpContextAccessor)
+    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
     {
         _logger = logger;
-        _httpContextAccessor = httpContextAccessor;
     }
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,

3fe4b1394d6f83af0b786816082eb3ed95d4b36b
philgei <philipp@geirhos.com>
2024-07-02 17:39:46 +0200

    Added Tests for Primitive H5P Mock

---
+++ AdLerBackend.Application.UnitTests/Common/ElementStrategies/MockPrimitiveH5PStrategyTest.cs
@@ -0,0 +1,33 @@
+﻿
+using AdLerBackend.Application.Common.ElementStrategies.ScoreElementStrategies.MockPrimitiveH5PStrategy;
+using FluentAssertions;
+
+namespace AdLerBackend.Application.UnitTests.Common.ElementStrategies.ScoreElementStrategies.MockPrimitiveH5PStrategy
+{
+    public class MockPrimitiveH5PStrategyCommandHandlerTests
+    {
+        private MockPrimitiveH5PStrategyCommandHandler _handler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _handler = new MockPrimitiveH5PStrategyCommandHandler();
+        }
+
+        [Test]
+        public async Task Handle_ValidCommand_ReturnsSuccessResponse()
+        {
+            // Arrange
+            var command = new MockPrimitiveH5PStrategyCommand
+            {
+                ElementId = 1
+            };
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+            
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+        }
+    }
+}

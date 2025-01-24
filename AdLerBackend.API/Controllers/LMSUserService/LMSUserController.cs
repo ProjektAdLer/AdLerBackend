de2494d7320d5ff5c1636d64a2a360a07f72c93a
Philipp Geirhos <philipp@geirhos.com>
2025-01-24 13:17:08 +0100

    Re-Added GetUserData endpoint

--- AdLerBackend.API/Controllers/LMSUserService/LMSUserController.cs
+++ AdLerBackend.API/Controllers/LMSUserService/LMSUserController.cs
@@ -1,5 +1,6 @@
 ﻿using AdLerBackend.Application.Common.Responses.LMSAdapter;
 using AdLerBackend.Application.LMS.GetLMSToken;
+using AdLerBackend.Application.LMS.GetUserData;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +11,16 @@ namespace AdLerBackend.API.Controllers.LMSUserService;
 [Route("api/Users")]
 public class LmsLoginController(IMediator mediator) : BaseApiController(mediator)
 {
+    [HttpGet("UserData")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(500)]
+    public async Task<ActionResult<LMSUserDataResponse>> GetLmsUserData(
+        [FromQuery] GetLMSUserDataCommand command)
+    {
+        return await Mediator.Send(command);
+    }
+
     [HttpGet("Login")]
     public async Task<ActionResult<LMSUserTokenResponse>> GetLmsUserToken(
         [FromQuery] GetLMSTokenCommand command)
